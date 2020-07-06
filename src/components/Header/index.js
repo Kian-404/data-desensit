d12b6210f9281834bdfb6ca232e37a7246d264b9
root <root@DESKTOP-3LLQIEM.localdomain>
2020-07-06 13:29:44 +0800

    Style: 修改header内容

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './index.less'
 import { connect } from 'react-redux'
 import { type } from '../../redux/action'
-import { Layout, Menu, Dropdown } from 'antd';
-import { MenuFoldOutlined, MenuUnfoldOutlined, DownOutlined, UserOutlined, InfoCircleOutlined, LogoutOutlined } from '@ant-design/icons'
+import { Layout, Menu, Dropdown, Row, Col, Divider } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined, BellOutlined, DownOutlined, UserOutlined, InfoCircleOutlined, LogoutOutlined } from '@ant-design/icons'
 const { Header } = Layout;
 
 
@@ -12,7 +12,7 @@ const LHeader = (props) => {
   console.log(props);
   const { showMenu, ToogleMenu } = props;
   const menu = (
-    <Menu onClick={(item) =>handleClickUser(item)}>
+    <Menu onClick={(item) => handleClickUser(item)}>
       <Menu.Item key="0">
         <InfoCircleOutlined />详情
       </Menu.Item>
@@ -21,10 +21,10 @@ const LHeader = (props) => {
       </Menu.Item>
     </Menu>
   );
-  const handleClickUser = ({key}) => {
+  const handleClickUser = ({ key }) => {
     console.log(key);
-    if(key === '1'){
-      window.location.href = '#/login';
+    if (key === '1') {
+      window.location.href = '/login';
     }
   }
   const Toggle = () => {
@@ -34,20 +34,37 @@ const LHeader = (props) => {
   let Icon = showMenu ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />;
   return (
     <Header className="header">
-      <div className="toogle-icon" onClick={Toggle}>
-        {Icon}
-      </div>
-      <div className="user-option">
-        <Dropdown overlay={menu}>
-          <div>
-            <UserOutlined /> <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-              admin <DownOutlined />
-            </a>
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col className="gutter-row" span={3}>
+          <div className="toogle-icon" onClick={Toggle}>
+            {Icon}
           </div>
+        </Col>
+        <Col className="gutter-row" span={17}>
+          <div >Header</div>
+        </Col>
+        <Col className="gutter-row" span={1}>
 
-        </Dropdown>,
-      </div>
-      Header</Header>
+        </Col>
+        <Col className="gutter-row" span={3}>
+          <div className="notice-option">
+            < BellOutlined />
+          </div>
+          <div className="user-option">
+            <Dropdown overlay={menu}>
+              <div>
+                <UserOutlined />
+                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                  admin <DownOutlined />
+                </a>
+              </div>
+            </Dropdown>
+          </div>
+        </Col>
+      </Row>
+
+
+    </Header>
   )
 }
 const mapStateToProps = state => {
